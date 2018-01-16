60a54a241a3fd16f42f0f2d3c3e493c325b8fffc
Pavel Alay <pavel.alay@duallab.com>
2018-01-16 16:38:51 +0300

    Update copyright header

--- src/test/java/com/itextpdf/pdfcleanup/BigDocumentAutoCleanUpTest.java
+++ src/test/java/com/itextpdf/pdfcleanup/BigDocumentAutoCleanUpTest.java
@@ -40,11 +40,6 @@
     For more information, please contact iText Software Corp. at this
     address: sales@itextpdf.com
  */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.itextpdf.pdfcleanup;
 
 import com.itextpdf.kernel.color.Color;
@@ -76,9 +71,6 @@ import org.junit.experimental.categories.Category;
 
 import static com.itextpdf.test.ITextTest.createOrClearDestinationFolder;
 
-/**
- * @author Joris Schellekens
- */
 @Category(IntegrationTest.class)
 public class BigDocumentAutoCleanUpTest {
 
@@ -253,6 +245,7 @@ class CustomLocationExtractionStrategy extends RegexBasedLocationExtractionStrat
         return colorByRectangle.containsKey(rect.getRectangle()) ? colorByRectangle.get(rect.getRectangle()) : Color.BLACK;
     }
 
+    @Override
     public ICleanupStrategy reset()
     {
         return new CustomLocationExtractionStrategy(regex);
