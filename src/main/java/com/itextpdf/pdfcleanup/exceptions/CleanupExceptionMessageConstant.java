7a6cfb7e7b68e8e127cb3506efc2a8f090801dd6
Nikita Kovaliov <nikita.kovaliov@duallab.com>
2020-11-26 12:54:46 +0300

    Fix SonarQube issues
    DEVSIX-4268
    
    (cherry picked from commit 8508b98e14dd9ea00f904617b407f7c98e62df62)

--- src/main/java/com/itextpdf/pdfcleanup/exceptions/CleanupExceptionMessageConstant.java
+++ src/main/java/com/itextpdf/pdfcleanup/exceptions/CleanupExceptionMessageConstant.java
@@ -27,6 +27,10 @@ package com.itextpdf.pdfcleanup.exceptions;
  */
 public final class CleanupExceptionMessageConstant {
     public static final String DEFAULT_APPEARANCE_NOT_FOUND = "DefaultAppearance is required but not found";
-    public static final String NONINVERTIBLE_MATRIX_CANNOT_BE_PROCESSED = "A noninvertible matrix has been parsed. The behaviour is unpredictable.";
-    public static final String PDF_DOCUMENT_MUST_BE_OPENED_IN_STAMPING_MODE = "PdfDocument must be opened in stamping mode.";
+    public static final String NONINVERTIBLE_MATRIX_CANNOT_BE_PROCESSED = "A noninvertible matrix has been parsed. "
+            + "The behaviour is unpredictable.";
+    public static final String PDF_DOCUMENT_MUST_BE_OPENED_IN_STAMPING_MODE = "PdfDocument must be opened in stamping "
+            + "mode.";
+
+    private CleanupExceptionMessageConstant(){}
 }
