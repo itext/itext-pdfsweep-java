72d0026fee5952e3fa6d2e042b85b1cdac13c186
Marco Andries <marco.andries@itextpdf.com>
2022-10-25 15:00:28 +0200

    Small change for testing

--- src/main/java/com/itextpdf/pdfcleanup/CleanUpProperties.java
+++ src/main/java/com/itextpdf/pdfcleanup/CleanUpProperties.java
@@ -40,6 +40,7 @@
     For more information, please contact iText Software Corp. at this
     address: sales@itextpdf.com
  */
+
 package com.itextpdf.pdfcleanup;
 
 import com.itextpdf.commons.actions.contexts.IMetaInfo;
